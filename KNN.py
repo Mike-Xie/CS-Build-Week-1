1f3aa12c640038f193ff3031f01be4b9cbb0b946
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-26 15:09:53 -0700

    create test data

---
+++ KNN.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import numpy as np 
+import scipy as sp 
+
+print("Hello, World!")
+
+data = {'name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'], 
+        'year': [2012, 2012, 2013, 2014, 2014], 
+        'reports': [4, 24, 31, 2, 3],
+        'coverage': [25, 94, 57, 62, 70]}
+df = pd.DataFrame(data, index = ['Cochice', 'Pima', 'Santa Cruz', 'Maricopa', 'Yuma'])
+
+df
+
+print(df)
