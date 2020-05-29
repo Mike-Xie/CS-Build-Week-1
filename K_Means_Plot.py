121fcf9722f21c959927915631ef81a440a82bda
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-28 20:02:16 -0700

    rename KNN to k_means, had wrong alg name

---
+++ K_Means_Plot.py
@@ -0,0 +1,29 @@
+from k_means import K_Means
+import numpy as np 
+
+X = np.array([[4,25],[24,94],[31,57],[2,62],[9,99],[3,70]])
+
+# instantiate and fit 
+clf = K_means(k=4)
+clf.fit(X)
+print(type(clf.centroids))
+print(clf.centroids)
+
+# plot centroids
+for centroid in clf.centroids:
+    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+                marker="o", color="k", s=150, linewidths=5)
+
+# clean up later, should be a 1 or 2 liner in Seaborn
+colors = ["g","r","b"]
+
+# plot classifications of points around centroids 
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidths=5)
+ 
+
+plt.show()
+end_time = time.time()
+print (f"runtime: {end_time - start_time} seconds")
--- k_means.py
+++ k_means.py
@@ -15,13 +15,6 @@ data = {'name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
         'coverage': [25, 94, 57, 62, 70]}
 df = pd.DataFrame(data, index = ['Cochice', 'Pima', 'Santa Cruz', 'Maricopa', 'Yuma'])
 
-X = np.array([[4,25],[24,94],[31,57],[2,62],[3,70]])
-# print(df)
-
-# print(distance.euclidean([1, 0, 0], [0, 1, 0]))
-# sns.barplot(x = "reports", y = "coverage", data=df)
-# plt.show()
-
 # create a class 
 class K_means:
 	def __init__(self, k: int=3, tolerance: float=0.001, max_iterations: int=100):
@@ -29,11 +22,10 @@ class K_means:
 		self.tolerance = tolerance
 		self.max_iterations = max_iterations
 
-	def fit(self, data):
+	def fit(self, data) -> None:
 		# put centroids in key value pair
 		self.centroids: Dict = {}
-		# assign some starting ones, non-randomized
-		# because easier to test 
+		# assign some starting ones, non-randomized, easier to test
 		for i in range(self.k):
 			self.centroids[i] = data[i]
 		# outer loop for max iterations
@@ -48,50 +40,28 @@ class K_means:
 				distance_list: List = [np.linalg.norm(item-self.centroids[centroid]) for centroid in self.centroids]
 				closest: int = distance_list.index(min(distance_list)) 
 				self.classifications[closest].append(item)
-			# make copy of old centroids
+			# compare old centroids to new centroids and quit
+			# method if the change is less than tolerance 
+
+			# create copy of old centroids to compare to new ones
 			prev_centroids = dict(self.centroids)
 			# update new centroids 
 			for classif in self.classifications:
 				self.centroids[classif] = np.average(self.classifications[classif], axis=0)
-			# code to exit if has converged to tolerance
+			# code to exit if has converged to within tolerance
 			is_stable: bool = True
 			for centroid in self.centroids:
 				old = prev_centroids[centroid]
 				curr = self.centroids[centroid]
-
 				if np.sum((curr - old)/old*100) > self.tolerance:
 					is_stable = False 
-
 			if is_stable:
 				break 
 
-	def predict(self, item):
+	def predict(self, item) -> int:
 		distance_list: List = [np.linalg.norm(item-self.centroids[centroid]) for centroid in self.centroids]
 		closest: int = distance_list.index(min(distance_list)) 
 		return closest 
 
-# instantiate and fit 
-clf = K_means(k=2)
-clf.fit(X)
-print(clf.centroids)
-
-# plot centroids
-for centroid in clf.centroids:
-    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
-                marker="o", color="k", s=150, linewidths=5)
-
-# clean up later, should be a 1 or 2 liner in Seaborn
-colors = ["g","r","b"]
-
-# plot classifications of points around centroids 
-for classification in clf.classifications:
-    color = colors[classification]
-    for featureset in clf.classifications[classification]:
-        plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidths=5)
- 
-
-plt.show()
-end_time = time.time()
-print (f"runtime: {end_time - start_time} seconds")
 
 
